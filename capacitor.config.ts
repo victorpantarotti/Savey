2dd209356bddb7b86383076a5e2c120e1fcecbe9
Fvictorpantarotti <victorpantarotti@gmail.com>
2024-10-19 20:50:09 -0300

    Simple Splash Screen for Ionic

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,22 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'br.com.victorpantarotti.savey',
   appName: 'Savey',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 1000,
+      backgroundColor: "#212121",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
